141f309840f4d7a47d384478b374f63609cb28d4
madushankaa <madushankakahawa@gmail.com>
2020-03-23 23:27:39 +0530

    add friend

--- data/add.js
+++ data/add.js
@@ -17,6 +17,14 @@ class Add{
             return null;
         }
     }
-    
+    async addFriend(id,friendId){
+        try{
+            await newUser.updateOne({_id : id},{$push:{friends:friendId}});
+            await newUser.updateOne({_id : friendId},{$push:{friends:id}});
+            return true;
+        }catch(e){
+            return null;
+        }
+    }
 }
 module.exports = Add;
