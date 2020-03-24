983ef73b7a3e73f0a921323b1c6bf40248371125
madushankaa <madushankakahawa@gmail.com>
2020-03-24 23:28:42 +0530

    for mysqldb connection

---
+++ data/sqldb.js
@@ -0,0 +1,16 @@
+// const mysql = require('mysql');
+// const Connect = () => {
+//     try{
+//         const connection = mysql.createConnection({
+//             host     : 'localhost',
+//             user     : 'root',
+//             password : '',
+//             database : 'test'
+//         });
+//         connection.connect();
+//         return true;
+//     }catch(e){
+//         return false;
+//     }
+// }
+// module.exports = Connect;
